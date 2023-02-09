82124f9268833390ea82ebdcb703b24a6c724167
Bareera Zakir <bxz111@student.bham.ac.uk>
2023-02-09 16:06:58 +0400

    some message

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    int gitWorkshop (int x, int y) {
+        for(int i = 0; i < 15; i++) {
+            System.out.println(x + y);
+        }
+        return 0;
+    }
+    public static void main (String [] args) {
+        System.out.println("Hello World");
+    }
+}
